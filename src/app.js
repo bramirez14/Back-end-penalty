1b0826b9c13bba57d3726f6d9d8184cdf441398d
Bryan Ramirez <bramirez@penaltyarg.com.ar>
2021-05-07 16:10:42 -0300

    pdf listo para usar demora 5seg

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const pdf = require('html-pdf');
 var cors = require('cors')
+const bodyParser = require('body-parser');
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,9 +21,12 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
- app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(cors());
+
+
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
@@ -39,5 +46,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
