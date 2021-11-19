04321a7c6eeae473bf51cdf7b841ecbf150c6fc3
Bryan Ramirez <bramirez@penaltyarg.com.ar>
2021-11-19 14:36:35 -0300

    scc

--- src/controller/zsccController.js
+++ src/controller/zsccController.js
@@ -1,53 +1,70 @@
 const path = require("path");
-const fs = require('fs-extra')
+const fs = require("fs-extra");
 const DB = require("../database/models_SQL");
-const {getConnection}= require("../sql/conexion")
-const sequelize= require('../sequelize_sql/config')
-const zsccController ={
-    todasZSCC: async (req, res) => {
-        try {
-            const pool= await getConnection();
-
-            const result = await pool.request().query('SELECT * FROM [WBT11_TEMP].[dbo].[Z_SCC]' );
-     
-          res.send(result.recordsets);
-
-        } catch (e) {
-            res.send(e)
-        }
-       
-    },
-idZSCC: async (req, res) => {
+const { getConnection } = require("../sql/conexion");
+const sequelize = require("../sequelize_sql/config");
+const zsccController = {
+  todasZSCC: async (req, res) => {
     try {
-        const {id}= req.params
-        const pool= await getConnection();
-        const result = await pool
+      const pool = await getConnection();
+
+      const result = await pool
         .request()
-        .query(`SELECT * FROM  [WBT11_TEMP].[dbo].[Z_SCC]  Where NROSCC = ${id}`);
+        .query("SELECT * FROM [WBT11_TEMP].[dbo].[Z_SCC]");
+
+      res.send(result.recordsets);
+    } catch (e) {
+      res.send(e);
+    }
+  },
+  idZSCC: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const pool = await getConnection();
+      const result = await pool
+        .request()
+        .query(
+          `SELECT * FROM  [WBT11_TEMP].[dbo].[Z_SCC]  Where NROSCC = ${id}`
+        );
       if (result.rowsAffected[0] === 0) return res.send(404);
       return res.send(result.recordset);
     } catch (e) {
-        res.send(e)
+      res.send(e);
     }
-},
-artZSCC:async (req, res) => {
+  },
+  artZSCC: async (req, res) => {
     try {
-        const pool= await getConnection();
-        
-        const result = await pool.request().query('SELECT NUMERO,CODTALLE FROM [WBT11_TEMP].[dbo].[ARTICULO]');
-     
-          res.send(result.recordsets);
+      const pool = await getConnection();
 
+      const result = await pool
+        .request()
+        .query(
+          "SELECT NUMERO,CODTALLE,DESCRIP FROM [WBT11_TEMP].[dbo].[ARTICULO]"
+        );
+
+      res.send(result.recordsets);
     } catch (e) {
-        res.send(e)
+      res.send(e);
     }
-}
+  },
+    talleZSCC: async (req, res) => {
+    try {
+      const pool = await getConnection();
 
+      const result = await pool
+        .request()
+        .query(
+          "SELECT * FROM [WBT11_TEMP].[dbo].[VW_TALLES]"
+        );
 
+      res.send(result.recordsets);
+    } catch (e) {
+      res.send(e);
+    }
+  },
 
-/*     guardarVacacion: async (req, res) => {},
+  /*     guardarVacacion: async (req, res) => {},
     editarVacacion: async (req, res) => {},
     eliminarVacacion: async (req, res) => {} */
-
-}
+};
 module.exports = zsccController;
--- src/routes/zscc.js
+++ src/routes/zscc.js
@@ -8,5 +8,7 @@ const zsccController = require('../controller/zsccController');
 router.get('/todos/registros',zsccController.todasZSCC);
 router.get('/:id',zsccController.idZSCC);
 router.get('/todos/articulos',zsccController.artZSCC);// traemos col numero y codtalle
+router.get('/todos/talles' ,zsccController.talleZSCC);// traemos las curva de talles
+
 
 module.exports = router;
