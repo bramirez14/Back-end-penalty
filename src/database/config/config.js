096396abb745db5f8b16adcfd71ea6d1a3c5923c
Administrador <administrador@penaltyarg.com.ar>
2022-06-25 05:16:10 -0300

    modificamos-config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,28 +1,28 @@
 module.exports = { 
-"development": {
-  "username": "root",
-     "password": "tucutucu",
-     "database": "Penalty",
-     "host": "127.0.0.1",
+  "development": {
+    "username": "root",
+     "password": "Cambuci14",
+     "database": "intranetpenalty",
+     "host": "181.45.252.13",
      "dialect": "mysql",
-   "define": {
-     "paranoid": true,
-   }
-
- },
-   "test": {
-     "username": "root",
-     "password": null,
-     "database": "database_development",
-     "host": "127.0.0.1",
-     "dialect": "mysql"
+     "define": {
+       "paranoid": true,
+     }
+  
    },
-   "production": {
-     "username": "root",
-     "password": null,
-     "database": "database_production",
-     "host": "127.0.0.1",
-     "dialect": "mysql"
+     "test": {
+       "username": "root",
+       "password": null,
+       "database": "database_development",
+       "host": "127.0.0.1",
+       "dialect": "mysql"
+     },
+     "production": {
+       "username": "root",
+       "password": null,
+       "database": "database_production",
+       "host": "127.0.0.1",
+       "dialect": "mysql"
+     }
    }
- }
- 
+   
