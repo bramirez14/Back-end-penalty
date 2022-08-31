98d022cdaef346220890203aa07241909e248c5d
BME <b.ramireeez@gmail.com>
2022-08-31 02:22:13 -0300

    comentando log

--- src/controller/reportesController.js
+++ src/controller/reportesController.js
@@ -112,11 +112,11 @@ const reportesController = {
           let data = [element.cliente,"DESPACHADO",element.fechafin, element.REMITO];
           await connection.query(sql, data, (error, results, fields) => {
             //if (error) return res.send(error.message);
-            console.log({
+            /* console.log({
               error,
               results,
               fields
-            });
+            }); */
             
           });
         }  
@@ -124,22 +124,22 @@ const reportesController = {
           let data = [element.cliente,"PREPARADO",element.fechafin, element.REMITO];
           await connection.query(sql, data, (error, results, fields) => {
             //if (error) return res.send(error.message);
-            console.log({
+            /* console.log({
               error,
               results,
               fields
-            });
+            }); */
           });
         }
         if(element.Estado === 'ACT'|| element.Estado === 'ACO'){
           let data = [element.cliente,"EN PREPARACION",element.fechafin, element.REMITO];
           await connection.query(sql, data, (error, results, fields) => {
           //  if (error) return res.send(error.message);
-          console.log({
+          /* console.log({
             error,
             results,
             fields
-          });
+          }); */
           });
         } 
       }
