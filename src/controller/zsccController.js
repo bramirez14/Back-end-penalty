3d495d29fbe80549bf456dbc1e0c17a37969ff2d
BME <b.ramireeez@gmail.com>
2023-04-19 22:54:37 -0300

    changes  of  file  scc  and used .env

--- src/controller/zsccController.js
+++ src/controller/zsccController.js
@@ -1,5 +1,5 @@
 const { getConnection } = require("../sql/conexion");
-require('dotenv').config()
+require('dotenv').config();
 const TABLE =process.env.TABLE
 const zsccController = {
   todasZSCC: async (req, res) => {
@@ -22,7 +22,7 @@ const zsccController = {
       const result = await pool
         .request()
         .query(
-          `SELECT * FROM  [WBT12].[dbo].[Z_SCC]  Where NROSCC = ${id}`
+          `SELECT * FROM  ${TABLE}.[Z_SCC]  Where NROSCC = ${id}`
         );
       if (result.rowsAffected[0] === 0) return res.send(404);
       return res.send(result.recordset);
@@ -37,7 +37,7 @@ const zsccController = {
       const result = await pool
         .request()
         .query(
-          "SELECT NUMERO,CODTALLE,DESCRIP FROM [WBT12].[dbo].[ARTICULO]"
+          `SELECT NUMERO,CODTALLE,DESCRIP FROM ${TABLE}.[ARTICULO]`
         );
 
       res.send(result.recordsets);
@@ -51,7 +51,7 @@ const zsccController = {
 
       const result = await pool
         .request()
-        .query("SELECT * FROM [WBT12].[dbo].[VW_TALLES]");
+        .query(`SELECT * FROM ${TABLE}.[VW_TALLES]`);
 
       res.send(result.recordsets);
     } catch (e) {
@@ -111,7 +111,7 @@ const zsccController = {
     try {
       const pool = await getConnection()
       const result = await pool.request()
-      .query(`SELECT top 50 * FROM [WBT12].[dbo].[PDCABEZA]`);
+      .query(`SELECT top 50 * FROM ${TABLE}.[PDCABEZA]`);
  res.send(result.recordset)
     } catch (e) {
       res.send({ msg: e, status: 400 });
@@ -122,11 +122,293 @@ delete: async (req, res) => {
     const { id } = req.params;
     const pool = await getConnection()
     const result = await pool.request()
-    .query(`DELETE FROM [WBT12].[dbo].[PDCABEZA] WHERE NROPED = ${id}`);
+    .query(`DELETE FROM ${TABLE}.[PDCABEZA] WHERE NROPED = ${id}`);
   } catch (e) {
     res.send(e)
   }
+},
+//NUEVA SCC 
+
+newSCC:async()=>{ 
+  try {
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let fecha;
+    if (month < 10) {
+      fecha = `${year}-0${month}-${day}`;
+    } else {
+      fecha = `${year}-${month}-${day}`;
+    }
+    const cantidadDeDigitos = (numero) => {
+      let longNumero = numero.toString().length;
+      let cantDeNumeros = 8;
+      let cantNumeroRestantes = cantDeNumeros - longNumero;
+      let contador = numero.toString();
+      if (longNumero >= cantDeNumeros) {
+        return numero;
+      } else {
+        for (let i = 0; i < cantNumeroRestantes; i++) {
+          contador = 0 + contador;
+        }
+        return contador;
+      }
+    };
+    let cantidadDeNUmeros =( digito ) => {
+      let long = digito.toString().length;
+      let cantnum= 3;
+      let restante = cantnum - long;
+      let cont = digito.toString();
+      if (long >= cantnum) {
+        return digito;
+      } else {
+        for (let i = 0; i < restante; i++) {
+          cont = 0 + cont;
+        }
+        return cont;
+      }
+      
+    }
+
+    const pool = await getConnection();
+    // aprobados
+    const filtroAprobados = await pool
+      .request()
+      .query(
+        `SELECT * FROM ${TABLE}.[Z_SCC] where APROBCRED='S' and APROBDEP='S' and  NROCOMP IS NULL`
+      );
+    const arrayApro = filtroAprobados.recordset.map((f) => f.CLIENTE);
+    //TE DA UN ARRAY DE SOLO NUMERO DE CLIENTES
+    const clientes = arrayApro.filter(function (ele, pos) {
+      return arrayApro.indexOf(ele) == pos;
+    });
+    //el numero de pedido
+    const numeroPedido = await pool
+      .request()
+      .query(
+        `SELECT CLAVE, FUNCION FROM ${TABLE}.[TABLASI09]  where CLAVE = 'SI091PD00006X' `
+      );
+
+    //insertamos en la tabla PDCABEZA
+    let number = numeroPedido.recordset[0].FUNCION;
+    for (let i = 0; i < clientes.length; i++) {
+      number++;
+      let buscarItemsClientes = filtroAprobados.recordset.filter(
+        (f) => f.CLIENTE === clientes[i]
+      );
+      let clienteActual = await pool
+        .request()
+        .query(
+          `SELECT * FROM ${TABLE}.[CLIENTES] WHERE NUMERO = '${clientes[i]}'`
+        );
+      let entrega = await pool
+        .request()
+        .query(
+          `SELECT TRANSPORTE FROM ${TABLE}.[CLTESENTREGA] WHERE CLIENTE= '${clientes[i]}' `
+        );
+      let codigo = await cantidadDeDigitos(number);
+
+      pool.request().query(`INSERT INTO ${TABLE}.[PDCABEZA] VALUES(
+        'P',
+        '${clientes[i]}',
+        '00006${codigo}',
+        '2C',
+        '${fecha}', 
+        '${fecha}', 
+        '00',
+        ${0},
+        '${clienteActual.recordset[0].DESCUENTO}',
+        ${0},
+        ${0},
+        '${clienteActual.recordset[0].VENDEDOR}',
+        '${entrega.recordset[0].TRANSPORTE}',
+        null,
+        'S',
+        '${clienteActual.recordset[0].DIRECCION}',
+        'L',
+        'L',
+        '01',
+        null,
+        '01',
+        null,
+        null,
+        null,
+        '${clienteActual.recordset[0].RAZONSOC}',
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        'N',
+        ${0},
+        null,
+        '${fecha}',
+        '${clienteActual.recordset[0].USUARIO}',
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        '00006${codigo}',
+        '01',
+        null,
+        null,
+        null,
+        'I',
+        null,
+        '1',
+        null
+        null)`);
+
+      for (let j = 0; j < buscarItemsClientes.length; j++) {
+      
+      let digito = await cantidadDeNUmeros([j + 1]);
+
+       await pool.request().query(`INSERT INTO ${TABLE}.[PDITEMS] 
+     VALUES (
+      'P', 
+      '${clientes[i]}',
+      '00006${codigo}',
+       '${digito}', 
+      '${buscarItemsClientes[j].ARTICULO}',
+      null,
+      '01',
+      '01',
+      ${0},
+      ${0},
+      ${buscarItemsClientes[j].PRECIO},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      '${buscarItemsClientes[j].CANTPED}',
+      '${buscarItemsClientes[j].CANTPED}',
+      '${buscarItemsClientes[j].CANTPEDT00}',
+      '${buscarItemsClientes[j].CANTPEDT01}',
+      '${buscarItemsClientes[j].CANTPEDT02}',
+      '${buscarItemsClientes[j].CANTPEDT03}',
+      '${buscarItemsClientes[j].CANTPEDT04}',
+      '${buscarItemsClientes[j].CANTPEDT05}',
+      '${buscarItemsClientes[j].CANTPEDT06}',
+      '${buscarItemsClientes[j].CANTPEDT07}',
+      '${buscarItemsClientes[j].CANTPEDT08}',
+      '${buscarItemsClientes[j].CANTPEDT09}',
+      '${buscarItemsClientes[j].CANTPEDT10}',
+      '${buscarItemsClientes[j].CANTPEDT11}',
+      '${buscarItemsClientes[j].CANTPEDT12}',
+      '${buscarItemsClientes[j].CANTPEDT13}',
+      '${buscarItemsClientes[j].CANTPEDT14}',
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      ${0},
+      'T3',
+      '0000000001',
+      '${fecha}',
+      null,
+      'N',
+      null,
+      null,
+      null,
+      null,
+      '${fecha}',
+      '${clienteActual.recordset[0].USUARIO}',
+      null,
+      null,
+      'N')`);
+
+      }
+
+      for (const item of buscarItemsClientes) {
+        await pool.request().query(`UPDATE ${TABLE}.[Z_SCC]  SET 
+      NROCOMP='00006${codigo}'  where NROSCC=${item.NROSCC}`);
+      }
+    }
+
+    await pool.request().query(`UPDATE ${TABLE}.[TABLASI09]  SET 
+    FUNCION = '${number}'  where CLAVE = 'SI091PD00006X'`);
+    res.send({msg:"scc comppletada",status:200});
+  } catch (e) {
+    res.send(e);
+  }
 }
+
+
+
 };
 module.exports = zsccController;
 
